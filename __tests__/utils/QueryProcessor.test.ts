a40699be61582c88a46620d12502c191f2a35ae4
Ztoyj <helenzheng1201@gmail.com>
2023-09-19 16:07:05 -0400

    added multiply

--- __tests__/utils/QueryProcessor.test.ts
+++ __tests__/utils/QueryProcessor.test.ts
@@ -25,4 +25,25 @@ describe("QueryProcessor", () => {
             "yujiazhe"
           ));
     });
+    test('should return sum', () => {
+        const query = "What is 69 plus 17?";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "86"
+          ));
+    });
+    test('should return max', () => {
+        const query = "Which of the following numbers is the largest: 47, 25, 72?";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "72"
+          ));
+    });
+    test('should return multiple', () => {
+        const query = "What is 93 multiplied by 35?";
+        const response: string = QueryProcessor(query);
+        expect(response).toBe((
+            "3255"
+          ));
+    });
 });
--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,6 +1,7 @@
 export default function QueryProcessor(query: string): string {
   const match1 = query.match(/What is (\d+) plus (\d+)\?/);
   const match2 = query.match(/Which of the following numbers is the largest: (\d+), (\d+), (\d+)\?/);
+  const match3 = query.match(/What is (\d+) multiplied by (\d+)\?/);
   if (query.toLowerCase().includes("shakespeare")) {
     return (
       "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
@@ -28,6 +29,11 @@ export default function QueryProcessor(query: string): string {
     const num2 = parseInt(match2[2], 10);
     const num3 = parseInt(match2[3], 10);
     return String(Math.max(Math.max(num1, num2), num3));
+  }
+  if (match3) {
+    const num1 = parseInt(match3[1], 10);
+    const num2 = parseInt(match3[2], 10);
+    return String(num1 * num2);
   }
   return "";
 }
