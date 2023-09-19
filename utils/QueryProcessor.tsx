71c335079bbde2ba5fb5387e8aefe0e25ba3f511
Ztoyj <helenzheng1201@gmail.com>
2023-09-19 15:43:55 -0400

    added my name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -11,6 +11,11 @@ export default function QueryProcessor(query: string): string {
       "yujiazhe"
     );
   }
+  if (query.toLowerCase().includes("name")) {
+    return (
+      "Helen"
+    );
+  }
 
   return "";
 }
